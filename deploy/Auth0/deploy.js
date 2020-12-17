43dea611aa322889fec59555ca726cbdd29450b9
MichaelJCompton <michael@dgraph.io>
2020-12-17 12:40:16 +1100

    Update Auth0 deployment script

--- deploy/Auth0/deploy.js
+++ deploy/Auth0/deploy.js
@@ -1,8 +1,68 @@
 const ManagementClient = require("auth0").ManagementClient
 const { readFileSync } = require("fs")
-require("dotenv").config()
+
+// NODE_ENV=production yarn run deploy-auth0
+// or
+// yarn run --prod deploy-auth0
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development"
+require("dotenv").config({ path: ".env." + env })
+const configFile = "config." + env + ".json"
 
 const dir = "deploy/Auth0"
+
+function addOrUpdateRule(management, config, ruleName) {
+  var rule = readFileSync(dir + "/rules/" + ruleName + ".js", "utf8")
+  rule = rule.replace(/<<app-claims-namespace>>/g, config.AUTH0_CUSTOM_CLAIMS)
+  rule = rule.replace(
+    /<<your-Slash-GraphQL-URL>>/g,
+    config.SLASH_GRAPHQL_ENDPOINT + "/graphql"
+  )
+  rule = rule.replace(
+    /<<your-M2M-hook>>/g,
+    "https://" + config.AUTH0_DOMAIN + "/oauth/token"
+  )
+  rule = rule.replace(/<<your-client-id>>/g, config.m2m.client_id)
+  rule = rule.replace(/<<your-client-secret>>/g, config.m2m.client_secret)
+  rule = rule.replace(
+    /<<your-M2M-audience>>/g,
+    "https://" + config.AUTH0_DOMAIN + "/api/v2/"
+  )
+
+  const ruleData = {
+    name: ruleName,
+    enabled: true,
+    script: rule,
+  }
+
+  management.getRules(function (err, rules) {
+    if (err) {
+      console.log(err)
+      process.exit(1)
+    }
+    const rule = rules.find((r) => r.name == ruleData.name)
+    if (rule) {
+      console.log("updating rule " + ruleData.name)
+
+      management.updateRule({ id: rule.id }, ruleData, function (err, _) {
+        if (err) {
+          console.log(err)
+          process.exit(1)
+        }
+        console.log("updated rule " + ruleData.name)
+      })
+    } else {
+      console.log("adding rule " + ruleData.name)
+      ruleData.stage = "login_success"
+      management.createRule(ruleData, function (err, _) {
+        if (err) {
+          console.log(err)
+          process.exit(1)
+        }
+        console.log("added rule " + ruleData.name)
+      })
+    }
+  })
+}
 
 function addOrUpdateHook(management, hookData, triggerId) {
   management.getHooks(function (err, hooks) {
@@ -36,72 +96,15 @@ function addOrUpdateHook(management, hookData, triggerId) {
   })
 }
 
-function addOrUpdateM2MHook(m2mConfig) {
-  var add_user_hook = readFileSync(
-    dir + "/hooks/" + "add-user-to-slash-graphql.js",
-    "utf8"
-  )
-  add_user_hook = add_user_hook.replace(
-    /<<your-M2M-hook>>/g,
-    "https://" + m2mConfig.domain + "/oauth/token"
-  )
-  add_user_hook = add_user_hook.replace(
-    /<<your-client-id>>/g,
-    m2mConfig.clientID
-  )
-  add_user_hook = add_user_hook.replace(
-    /<<your-client-secret>>/g,
-    m2mConfig.clientSecret
-  )
-  add_user_hook = add_user_hook.replace(
-    /<<your-M2M-audience>>/g,
-    "https://" + m2mConfig.domain + "/api/v2/"
-  )
-  add_user_hook = add_user_hook.replace(
-    /<<your-Slash-GraphQL-URL>>/g,
-    process.env.REACT_APP_SLASH_GRAPHQL_ENDPOINT + "/graphql"
-  )
-
-  var add_user_hook_data = {
-    name: "add-user-to-slash-graphql",
-    script: add_user_hook,
-    enabled: true,
-  }
-  addOrUpdateHook(management, add_user_hook_data, "post-user-registration")
+function rules(management, config) {
+  addOrUpdateRule(management, config, "add-username")
+  addOrUpdateRule(management, config, "add-user-to-slash-graphql")
+  // add a rule like this if you want to do JWT enrichment during login
+  // addOrUpdateRule(management, config, "enrich-jwt-from-slash-graphql")
 }
 
-function addOrUpdateRule(management, ruleData, stage) {
-  management.getRules(function (err, rules) {
-    if (err) {
-      console.log(err)
-      process.exit(1)
-    }
-    const rule = rules.find((r) => r.name == ruleData.name)
-    if (rule) {
-      console.log("updating rule " + ruleData.name)
-
-      management.updateRule({ id: rule.id }, ruleData, function (err, _) {
-        if (err) {
-          console.log(err)
-          process.exit(1)
-        }
-        console.log("updated rule " + ruleData.name)
-      })
-    } else {
-      console.log("adding rule " + ruleData.name)
-      ruleData.stage = "login_success"
-      management.createRule(ruleData, function (err, _) {
-        if (err) {
-          console.log(err)
-          process.exit(1)
-        }
-        console.log("added rule " + ruleData.name)
-      })
-    }
-  })
-}
-
-var config = JSON.parse(readFileSync(dir + "/config.json", "utf8"))
+var config = JSON.parse(readFileSync(dir + "/" + configFile, "utf8"))
+config.SLASH_GRAPHQL_ENDPOINT = process.env.REACT_APP_SLASH_GRAPHQL_ENDPOINT
 
 var management = new ManagementClient({
   domain: config.AUTH0_DOMAIN,
@@ -109,49 +112,45 @@ var management = new ManagementClient({
   clientSecret: config.AUTH0_CLIENT_SECRET,
 })
 
-var authorize_add_hook = readFileSync(
-  dir + "/hooks/" + "authorize-add-user-to-slash-graphql.js",
-  "utf8"
-)
-authorize_add_hook = authorize_add_hook.replace(
+var auth0_hook = readFileSync(dir + "/hooks/" + "authorize-add-user-to-slash-graphql.js", "utf8")
+auth0_hook = auth0_hook.replace(
   /<<app-claims-namespace>>/g,
   config.AUTH0_CUSTOM_CLAIMS
 )
-var authorize_add_hook_data = {
-  name: "authorize-add-user-to-slash-graphql",
-  script: authorize_add_hook,
+var auth0_hook_data = {
+  name: "add-auth-role",
+  script: auth0_hook,
   enabled: true,
 }
-addOrUpdateHook(management, authorize_add_hook_data, "credentials-exchange")
-
-var add_username_rule = readFileSync(
-  dir + "/rules/" + "add-username.js",
-  "utf8"
-)
-add_username_rule = add_username_rule.replace(
-  /<<app-claims-namespace>>/g,
-  config.AUTH0_CUSTOM_CLAIMS
-)
-var add_username_rule_data = {
-  name: "add-username",
-  enabled: true,
-  script: add_username_rule,
-}
-addOrUpdateRule(management, add_username_rule_data)
+addOrUpdateHook(management, auth0_hook_data, "credentials-exchange")
 
 management.getClients(function (err, clients) {
   if (err) {
     console.log(err)
     process.exit(1)
   }
-  const m2m = clients.find((c) => c.name == "Authorize M2M for Slash GraphQL")
 
-  if (m2m) {
-    addOrUpdateM2MHook({
-      clientID: m2m.client_id,
-      clientSecret: m2m.client_secret,
-      domain: config.AUTH0_DOMAIN,
+  const spa = clients.find((c) => c.name == config.AUTH0_APP)
+  if (!spa) {
+    var spaData = JSON.parse(
+      readFileSync(dir + "/clients/" + config.AUTH0_APP + ".json", "utf8")
+    )
+    spaData.name = config.AUTH0_APP
+    management.createClient(spaData, function (err) {
+      if (err) {
+        console.log(err)
+        process.exit(1)
+      }
+      console.log("created application " + config.AUTH0_APP)
     })
+  } else {
+    console.log("application " + config.AUTH0_APP + " already exists")
+  }
+
+  const m2m = clients.find((c) => c.name == "Authorize M2M for Slash GraphQL")
+  if (m2m) {
+    config.m2m = m2m
+    rules(management, config)
   } else {
     var m2mClient = JSON.parse(
       readFileSync(
@@ -164,13 +163,10 @@ management.getClients(function (err, clients) {
         console.log(err)
         process.exit(1)
       }
-      console.log('created application "Authorize M2M for Slash GraphQL"')
+      console.log('created "Authorize M2M for Slash GraphQL"')
 
-      addOrUpdateM2MHook({
-        clientID: createdM2MClient.client_id,
-        clientSecret: createdM2MClient.client_secret,
-        domain: config.AUTH0_DOMAIN,
-      })
+      config.m2m = createdM2MClient
+      rules(management, config)
 
       const grantData = {
         client_id: createdM2MClient.client_id,
@@ -188,22 +184,5 @@ management.getClients(function (err, clients) {
         )
       })
     })
-  }
-
-  const spa = clients.find((c) => c.name == config.AUTH0_APP)
-  if (!spa) {
-    var spaData = JSON.parse(
-      readFileSync(dir + "/clients/" + "MyApp.json", "utf8")
-    )
-    spaData.name = config.AUTH0_APP
-    management.createClient(spaData, function (err) {
-      if (err) {
-        console.log(err)
-        process.exit(1)
-      }
-      console.log('created application ' + config.AUTH0_APP)
-    })
-  } else {
-    console.log('application ' + config.AUTH0_APP + ' already exists')
   }
 })
