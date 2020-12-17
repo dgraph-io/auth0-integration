1b40f64847b1017eef2be3b2156d797345cd372d
MichaelJCompton <michael@dgraph.io>
2020-12-17 11:50:29 +1100

    update add user

--- deploy/Auth0/hooks/authorize-add-user-to-slash-graphql.js
+++ deploy/Auth0/hooks/authorize-add-user-to-slash-graphql.js
@@ -16,19 +16,18 @@
 // application so you can get a client ID and secret to
 // call this hook (see add-user-to-slash-graphql.js for how it's called).
 //
-// This gets called by Auth0 "Post User Registration" to generate
+// This gets called by the Auth0 login flow on a first login to generate
 // a special JWT that has the permission to add a new user to Slash GraphQL.
 //
-// There's no way to get such a JWT except via this code, and it
-// can only be run by the Post User Registration in Auth0 (or
-// if you know the Auth0 secrets), so adding users is a secured
-// flow in the app and can only happen for users who sign up
-// using the Auth0 flow.
+// There's no way to get such a JWT except via this code, and it's
+// only run by Auth0 (or if you know the Auth0 client secrets), 
+// so adding users is a secured flow in the app and can only happen for 
+// users who sign up using the Auth0 flow.
 
 module.exports = function (client, scope, audience, context, cb) {
   var access_token = {}
   access_token.scope = scope
-  access_token["<<app-claims-namespace>>"] = { role: "AddUser" }
+  access_token["<<app-claims-namespace>>"] = { role: "Admin" }
 
   cb(null, access_token)
 }
